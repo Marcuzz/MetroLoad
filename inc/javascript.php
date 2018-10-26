938af531d04b06235fafb25fd8e77f5536d19731
Marcus <marcuz1111@gmail.com>
2018-10-26 14:15:18 +0200

    Fix youtube audio

--- inc/javascript.php
+++ inc/javascript.php
@@ -1,5 +1,37 @@
 <script>
-		function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode ) 
+		$(function(){
+			$('#music').on('ended', function(){
+				var curName = $('.songName').text();
+				var curSrc = $(this).attr('src');
+				
+				// find next and put old at end
+				var next = $(this).next();
+				var html = next.clone();
+				$(html).find('.file').text(curSrc);
+				$(html).find('.name').text(curName);
+				$(this).parent().append(html);
+
+				// replace old values with new and remove new
+				$(this).attr('src', $(next).find('.file').text());
+				$('.songName').text($(next).find('.name').text());
+				$(next).remove();
+
+				this.currentTime = 0;
+				this.play();
+			});
+		});
+
+		function setVolume()
+		{
+			music=document.getElementById("music");
+			music.volume=<?php echo $music_volume; ?>;
+		}
+		
+		$(function () {
+			setVolume();
+		});
+
+		function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode )
 		{
 			var servername=servername
 			var gamemode=gamemode
@@ -8,18 +40,18 @@
 			document.getElementById("gamemode").innerHTML=gamemode;
 			document.getElementById("maxplayers").innerHTML=maxplayers;
 		}
-		
+
 		function SetStatusChanged( status )
 		{
 			var status=status;
 			document.getElementById("status").innerHTML=status;
 		}
-		function SetFilesTotal( total ) 
+		function SetFilesTotal( total )
 		{
 			var total=total;
 			document.getElementById("total").innerHTML=total;
 		}
-		function SetFilesNeeded( needed ) 
+		function SetFilesNeeded( needed )
 		{
 			var needed=needed;
 			document.getElementById("needed").innerHTML=needed;
@@ -28,19 +60,19 @@
 		{
 			var dfile=fileName;
 			document.getElementById("fileDL").innerHTML=dfile;
-			
+
 			var total = document.getElementById('total').innerHTML;
 			var needed = document.getElementById('needed').innerHTML;
 			CalcPercentage(parseInt(total), parseInt(needed));
 		}
-		function CalcPercentage(total, needed){						
+		function CalcPercentage(total, needed){
 			var perc=Math.round((needed/total)*100);
 			var percent = 100-perc;
-			
+
 			document.getElementById("progressbar").style.width=percent+'%';
 			document.getElementById("percent").innerHTML=percent;
 		}
-		
+
 		<?php if($enable_cycle_colours && !$enable_img_cycling){ ?>
 		$(function () {
 			var colors = <?php echo $colours_l; ?>;
@@ -50,7 +82,7 @@
 			cont.css('backgroundColor', colors[0]);
 			cont.css('backgroundColor', colors[1]);
 			anim();
-			
+
 				function anim() {
 					if (i == colors.length - 1) {
 						i=0;
@@ -69,7 +101,7 @@
 				}
 		});
 		<?php } ?>
-		
+
 		<?php if($enable_img_cycling && !$enable_cycle_colours){ ?>
 		$(function () {
 			$('.slideshow img:gt(0)').hide();
@@ -83,11 +115,4 @@
 			}, <?php echo $img_cycling_timer; ?>);
 		});
 		<?php } ?>
-		
-		function setVolume()
-		{
-			music=document.getElementById("music");
-			music.volume=<?php echo $music_volume; ?>;
-		}
-		window.onload=setVolume;
 </script>
