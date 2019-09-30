ee1c4ab9be0a353bc52954355d43a20f5947cb83
Marcuz <marcuz1111@gmail.com>
2019-09-30 22:54:00 +0200

    refactor JS

--- inc/javascript.php
+++ inc/javascript.php
@@ -1,118 +1,111 @@
 <script>
-		$(function(){
-			$('#music').on('ended', function(){
-				var curName = $('.songName').text();
-				var curSrc = $(this).attr('src');
-				
-				// find next and put old at end
-				var next = $(this).next();
-				var html = next.clone();
-				$(html).find('.file').text(curSrc);
-				$(html).find('.name').text(curName);
-				$(this).parent().append(html);
+$(function () {
+  $('#music').on('ended', function () {
+    let curName = $('.songName').text();
+    let curSrc = $(this).attr('src');
 
-				// replace old values with new and remove new
-				$(this).attr('src', $(next).find('.file').text());
-				$('.songName').text($(next).find('.name').text());
-				$(next).remove();
+    // find next and put old at end
+    let next = $(this).next();
+    let html = next.clone();
+    $(html).find('.file').text(curSrc);
+    $(html).find('.name').text(curName);
+    $(this).parent().append(html);
 
-				this.currentTime = 0;
-				this.play();
-			});
-		});
+    // replace old values with new and remove new
+    $(this).attr('src', $(next).find('.file').text());
+    $('.songName').text($(next).find('.name').text());
+    $(next).remove();
 
-		function setVolume()
-		{
-			music=document.getElementById("music");
-			music.volume=<?php echo $music_volume; ?>;
-		}
-		
-		$(function () {
-			setVolume();
-		});
+    this.currentTime = 0;
+    this.play();
+  });
+});
 
-		function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode )
-		{
-			var servername=servername
-			var gamemode=gamemode
-			var maxplayers=maxplayers
-			document.getElementById("servername").innerHTML=servername;
-			document.getElementById("gamemode").innerHTML=gamemode;
-			document.getElementById("maxplayers").innerHTML=maxplayers;
-		}
+function setVolume() {
+  music = document.getElementById("music");
+  music.volume =<?php echo $music_volume; ?>;
+}
 
-		function SetStatusChanged( status )
-		{
-			var status=status;
-			document.getElementById("status").innerHTML=status;
-		}
-		function SetFilesTotal( total )
-		{
-			var total=total;
-			document.getElementById("total").innerHTML=total;
-		}
-		function SetFilesNeeded( needed )
-		{
-			var needed=needed;
-			document.getElementById("needed").innerHTML=needed;
-		}
-		function DownloadingFile( fileName )
-		{
-			var dfile=fileName;
-			document.getElementById("fileDL").innerHTML=dfile;
+$(function () {
+  setVolume();
+});
 
-			var total = document.getElementById('total').innerHTML;
-			var needed = document.getElementById('needed').innerHTML;
-			CalcPercentage(parseInt(total), parseInt(needed));
-		}
-		function CalcPercentage(total, needed){
-			var perc=Math.round((needed/total)*100);
-			var percent = 100-perc;
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
+  document.getElementById("servername").innerHTML = servername;
+  document.getElementById("gamemode").innerHTML = gamemode;
+  document.getElementById("max-players").innerHTML = maxplayers;
+}
 
-			document.getElementById("progressbar").style.width=percent+'%';
-			document.getElementById("percent").innerHTML=percent;
-		}
+function SetStatusChanged(status) {
+  document.getElementById("status").innerHTML = status;
+}
 
-		<?php if($enable_cycle_colours && !$enable_img_cycling){ ?>
-		$(function () {
-			var colors = <?php echo $colours_l; ?>;
-			var i = 0;
-			var cont = $('body.bg');
-			cont.css('opacity', 1);
-			cont.css('backgroundColor', colors[0]);
-			cont.css('backgroundColor', colors[1]);
-			anim();
+function SetFilesTotal(total) {
+  document.getElementById("total").innerHTML = total;
+}
 
-				function anim() {
-					if (i == colors.length - 1) {
-						i=0;
-					}
-					cont.css({
-						backgroundColor: colors[i],
-						opacity: 1
-					});
-					cont.css({
-						backgroundColor: colors[i+1]
-					});
-					i++;
-					cont.stop().animate({
-						opacity: 1
-					}, 2000, anim);
-				}
-		});
-		<?php } ?>
+function SetFilesNeeded(needed) {
+  document.getElementById("needed").innerHTML = needed;
+}
 
-		<?php if($enable_img_cycling && !$enable_cycle_colours){ ?>
-		$(function () {
-			$('.slideshow img:gt(0)').hide();
+function DownloadingFile(fileName) {
+  document.getElementById("file-dl").innerHTML = fileName;
 
-			setInterval(function () {
-				$('.slideshow :first-child').fadeOut()
-										 .next('img')
-										 .fadeIn()
-										 .end()
-										 .appendTo('.slideshow');
-			}, <?php echo $img_cycling_timer; ?>);
-		});
-		<?php } ?>
+  let total = document.getElementById('total').innerHTML;
+  let needed = document.getElementById('needed').innerHTML;
+  CalcPercentage(parseInt(total), parseInt(needed));
+}
+
+function CalcPercentage(total, needed) {
+  let perc = Math.round((needed / total) * 100);
+  let percent = 100 - perc;
+
+  document.getElementById("progressbar").style.width = percent + '%';
+  document.getElementById("percent").innerHTML = percent;
+}
+
+<?php if($enable_cycle_colours && !$enable_img_cycling){ ?>
+  $(function () {
+    let colors = <?php echo $colours_l; ?>;
+    let i = 0;
+    let cont = $('body.bg');
+
+    cont.css('opacity', 1);
+    cont.css('backgroundColor', colors[0]);
+    cont.css('backgroundColor', colors[1]);
+
+    anim();
+
+    function anim() {
+      if (i === colors.length - 1) {
+        i = 0;
+      }
+      cont.css({
+        backgroundColor: colors[i],
+        opacity: 1
+      });
+      cont.css({
+        backgroundColor: colors[i + 1]
+      });
+      i++;
+      cont.stop().animate({
+        opacity: 1
+      }, 2000, anim);
+    }
+  });
+  <?php } ?>
+
+  <?php if($enable_img_cycling && !$enable_cycle_colours){ ?>
+  $(function () {
+    $('.slideshow img:gt(0)').hide();
+
+    setInterval(function () {
+      $('.slideshow :first-child').fadeOut()
+          .next('img')
+          .fadeIn()
+          .end()
+          .appendTo('.slideshow');
+    }, <?php echo $img_cycling_timer; ?>);
+  });
+<?php } ?>
 </script>
